a3cce84a77ecac7db79230219be67be6191af67e
barbareshka <119070110+barbareshka@users.noreply.github.com>
2022-12-23 18:58:26 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -6,28 +6,7 @@ import matplotlib.pylab as plt
 import torch
 import shutil
 
-
-class Config(dict):
-    def __init__(self, *args, **kwargs):
-        super(config, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-
-def load_checkpoint(filepath, device):
-    assert os.path.isfile(filepath)
-    print("Loading '{}'".format(filepath))
-    checkpoint_dict = torch.load(filepath, map_location=device)
-    print("Complete.")
-    return checkpoint_dict
-
-
-def save_checkpoint(filepath, obj):
-    print("Saving checkpoint to {}".format(filepath))
-    torch.save(obj, filepath)
-    print("Complete.")
-
-
-def scan_checkpoint(cp_dir, prefix):
+def scan(cp_dir, prefix):
     pattern = os.path.join(cp_dir, prefix + '????????')
     cp_list = glob.glob(pattern)
     if len(cp_list) == 0:
