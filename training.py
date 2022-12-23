8136bdea2c4a3272b2a868cb08d5aaa722e0b3ab
barbareshka <119070110+barbareshka@users.noreply.github.com>
2022-12-23 19:41:25 +0300

    Update training.py

--- training.py
+++ training.py
@@ -16,8 +16,8 @@ from torch.distributed import init_process_group
 from torch.nn.parallel import DistributedDataParallel
 
 from meldataset import MelDataset, MelTunedDataset, mel_spectrogram, get_dataset_filelist
-from hifigan import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss,\
-    discriminator_loss
+from hifigan import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator
+from losses import f_loss, g_loss, d_loss
 from tools import scan_checkpoint
 
 torch.backends.cudnn.benchmark = True
@@ -137,11 +137,11 @@ def train(a, h):
 
             # MPD
             y_df_hat_r, y_df_hat_g, _, _ = mpd(y, y_g_hat.detach())
-            loss_disc_f, losses_disc_f_r, losses_disc_f_g = discriminator_loss(y_df_hat_r, y_df_hat_g)
+            loss_disc_f, losses_disc_f_r, losses_disc_f_g = d_loss(y_df_hat_r, y_df_hat_g)
 
             # MSD
             y_ds_hat_r, y_ds_hat_g, _, _ = msd(y, y_g_hat.detach())
-            loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(y_ds_hat_r, y_ds_hat_g)
+            loss_disc_s, losses_disc_s_r, losses_disc_s_g = d_loss(y_ds_hat_r, y_ds_hat_g)
 
             loss_disc_all = loss_disc_s + loss_disc_f
 
@@ -156,10 +156,10 @@ def train(a, h):
 
             y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
             y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
-            loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
-            loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
-            loss_gen_f, losses_gen_f = generator_loss(y_df_hat_g)
-            loss_gen_s, losses_gen_s = generator_loss(y_ds_hat_g)
+            loss_fm_f = f_loss(fmap_f_r, fmap_f_g)
+            loss_fm_s = f_loss(fmap_s_r, fmap_s_g)
+            loss_gen_f, losses_gen_f = g_loss(y_df_hat_g)
+            loss_gen_s, losses_gen_s = g_loss(y_ds_hat_g)
             loss_gen_all = loss_gen_s + loss_gen_f + loss_fm_s + loss_fm_f + loss_mel
 
             loss_gen_all.backward()
