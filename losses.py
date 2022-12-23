98f35b0346371293462ed4944f3eab95dd4e2130
barbareshka <119070110+barbareshka@users.noreply.github.com>
2022-12-23 21:22:16 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,16 +2,15 @@ import torch
 
 def d_loss(real, gen):
     total = 0.
-    losses_r = []
-    Losses_g = []
+    reals = []
+    gens = []
     for e1, e2 in zip(real, gen):
         l_r = torch.mean((1 - e1) ** 2)
         l_g = torch.mean(e2 ** 2)
-        losses_r.append(l_r.item())
-        losses_g.append(l_g.item())
-        total += (r_loss + g_loss)
-
-    return total, r_losses, g_losses
+        reals.append(l_r.item())
+        gens.append(l_g.item())
+        total += (l_r + l_g)
+    return total, reals, gens
 
 
 def g_loss(res):
@@ -21,7 +20,6 @@ def g_loss(res):
         loss = torch.mean((1 - _) ** 2)
         losses.append(loss)
         total += loss
-
     return total, losses
 
 
